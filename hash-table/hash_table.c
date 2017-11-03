b9cad8dbe73c58fb14f5258c82e71cacb11d0fee
James Thomas <codeactor@protonmail.com>
2017-11-03 07:08:56 +0000

    delete individual key in linear probed hash table

--- hash-table/hash_table.c
+++ hash-table/hash_table.c
@@ -8,6 +8,12 @@ Record **new_hashtable(int hashtable_size)
 {
     Record **hashtable = malloc(sizeof(Record *) * hashtable_size);
 
+    if (hashtable == NULL)
+    {
+        fprintf(stderr, "Failed to allocate memory for hashtable\n");
+        exit(1);
+    }
+
     for (int record = 0; record < hashtable_size; record++)
     {
             hashtable[record] = NULL;
@@ -23,7 +29,7 @@ Record *new_record(void *key, void *value)
 
     if (r == NULL)
     {
-        printf("Failed to allocate memory\n");
+        fprintf(stderr, "Failed to allocate memory for record\n");
         exit(1);
     }
 
@@ -70,6 +76,48 @@ Record *search(Record **hashtable,
    }
 
    return NULL;
+}
+
+
+void delete_key(Record **hashtable,
+                int hashtable_size,
+                void *key,
+                bool (*cmp_ptr)(void *, void *),
+                unsigned int (*hash_ptr)(void *))
+{
+    unsigned int pos = hash_ptr(key) % hashtable_size;
+
+    while (hashtable[pos] != NULL)
+    {
+            if(cmp_ptr(key, hashtable[pos]->key))
+            {
+                    break;
+            }
+            else
+            {
+                    pos = (pos + 1) % hashtable_size;
+            }
+
+            if (hashtable[pos] == NULL)
+            {
+                    return;
+            }
+
+            free(hashtable[pos]);
+            hashtable[pos] = NULL;
+    }
+
+    // re-insert elements so no empty cells for probing
+    // slow, so consider switching to a flag "empty but deleted"
+    // http://www.cs.rmit.edu.au/online/blackboard/chapter/05/documents/contribute/chapter/05/chaining.html
+    for (pos = (pos + 1) % hashtable_size;
+         hashtable[pos] != NULL;
+         pos = (pos + 1) % hashtable_size)
+    {
+            Record *r = hashtable[pos];
+            hashtable[pos] = NULL;
+            insert(hashtable, r, hashtable_size, hash_ptr);
+    }
 }
 
 
