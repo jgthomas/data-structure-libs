fe24f3700224ea7145c898cf7361c1245f91675e
James Thomas <codeactor@protonmail.com>
2017-11-05 20:18:01 +0000

    cleaned up files

---
+++ binary-search-tree/binary_tree.h
@@ -0,0 +1,12 @@
+typedef struct new_node {
+        void *data;
+        struct new_node *left;
+        struct new_node *right;
+}
+BinTreeNode;
+
+BinTreeNode *make_node(void *new_data, size_t data_size);
+void insert(BinTreeNode **head, void *new_data, size_t data_size, bool (*less)(void *first, void *second));
+bool search(BinTreeNode **head, void *data, bool (*equals)(void *first, void *second), bool (*less)(void *first, void *second));
+void print_tree(BinTreeNode *node, int level, void (*print_ptr)(void *item));
+void delete_tree(BinTreeNode *head);
--- binary-search-tree/bintree_funcs.h
+++
@@ -1,5 +0,0 @@
-Node *make_node(void *new_data, size_t data_size);
-void insert(Node **head, void *new_data, size_t data_size, bool (*less)(void *first, void *second));
-bool search(Node **head, void *data, bool (*equals)(void *first, void *second), bool (*less)(void *first, void *second));
-void print_tree(Node *node, int level, void (*print_ptr)(void *item));
-void delete_tree(Node *head);
