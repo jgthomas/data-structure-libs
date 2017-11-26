9157cf5c261e0e8adb83697b2fef80c40accfd27
James Thomas <codeactor@protonmail.com>
2017-11-26 19:27:31 +0000

    include guards added

--- binary-search-tree/binary_tree.h
+++ binary-search-tree/binary_tree.h
@@ -1,3 +1,6 @@
+#ifndef BINARY_TREE_H
+#define BINARY_TREE_H 
+
 typedef struct new_node {
         void *data;
         struct new_node *left;
@@ -10,3 +13,5 @@ void insert(BinTreeNode **head, void *new_data, size_t data_size, bool (*less)(v
 bool search(BinTreeNode **head, void *data, bool (*equals)(void *first, void *second), bool (*less)(void *first, void *second));
 void print_tree(BinTreeNode *node, int level, void (*print_ptr)(void *item));
 void delete_tree(BinTreeNode *head);
+
+#endif
