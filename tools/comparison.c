b19cb576890bac6e7cc650bfa56f165b264def22
James Thomas <codeactor@protonmail.com>
2017-11-28 19:15:09 +0000

    fixed broken char comparison

--- tools/comparison.c
+++ tools/comparison.c
@@ -42,7 +42,7 @@ bool less_than_char(void *first, void *second)
 
 bool more_than_char(void *first, void *second)
 {
-    return *(int *)first > *(int *)second;
+    return (int)*(char *)first > (int)*(char *)second;
 }
 
 bool equal_char(void *search, void *node_data)
