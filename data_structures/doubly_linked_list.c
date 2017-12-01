1a38be4855ae5c6061a3fab6b7de7cbb9e74010c
James Thomas <codeactor@protonmail.com>
2017-12-01 08:16:25 +0000

    formatting tweak

--- data_structures/doubly_linked_list.c
+++ data_structures/doubly_linked_list.c
@@ -38,27 +38,27 @@ void list_push(Node **head, void *new_data, size_t data_size)
 
 void append(Node **node, void *new_data, size_t data_size)
 {
-    // create new node, set next to NULL as last
-    Node *new_node = create_node(new_data);;
-    new_node->next = NULL;
+        // create new node, set next to NULL as last
+        Node *new_node = create_node(new_data);;
+        new_node->next = NULL;
 
-    // set up cursor
-    Node *node_ptr = (*node);
+        // set up cursor
+        Node *node_ptr = (*node);
 
-    // advance to end of list
-    while (node_ptr->next != NULL)
-    {
-        node_ptr = node_ptr->next;
-    }
+        // advance to end of list
+        while (node_ptr->next != NULL)
+        {
+                node_ptr = node_ptr->next;
+        }
 
-    // set previous to current end node
-    new_node->previous = node_ptr;
+        // set previous to current end node
+        new_node->previous = node_ptr;
 
-    // if end node exists, point next to new node
-    if (node_ptr != NULL)
-    {
-        node_ptr->next = new_node;
-    }
+        // if end node exists, point next to new node
+        if (node_ptr != NULL)
+        {
+                node_ptr->next = new_node;
+        }
 }
 
 
