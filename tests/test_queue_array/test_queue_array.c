8e9ca858fa8fdc4eacfa04b24ea84505a34ec477
James Thomas <codeactor@protonmail.com>
2017-12-03 10:09:10 +0000

    proper unit tests for queue array implementation

--- tests/test_queue_array/test_queue_array.c
+++ tests/test_queue_array/test_queue_array.c
@@ -6,36 +6,108 @@
 #include "tests/shared_test.h"
 #include "tools/printing.h"
 #include "tools/comparison.h"
-#include "tools/convertors.h"
 #include "data_structures/queue_array.h"
 
 
 enum {NUM_TESTS = 3};
 
 
-void testBASIC_QUEUE_OPERATIONS(void)
+int test_int[] = {15,13,46,10,1};
+int answer_int[] = {1,10,46,13,15};
+
+char test_char[] = {'e','a','r','t','b'};
+char answer_char[] = {'b','t','r','a','e'};
+
+char *test_string[] = {"zebra",
+                       "moose",
+                       "elephant",
+                       "armadillo",
+                       "coyote"};
+
+char *answer_string[] = {"coyote",
+                         "armadillo",
+                         "elephant",
+                         "moose",
+                         "zebra"};
+int LENGTH = 5;
+
+
+TestCase **make_tests(int num_tests)
+{        
+        TestCase **test_array = init_tests(num_tests);
+
+        TestCase *testint = new_test(test_int, answer_int, sizeof(test_int), sizeof(int), equal_int, less_than_int, print_int);
+        TestCase *testchar = new_test(test_char, answer_char, sizeof(test_char), sizeof(char), equal_char, less_than_char, print_char);
+        TestCase *teststring = new_test(test_string, answer_string, sizeof(test_string), sizeof(char *), equal_string, less_than_string, print_string);
+
+        test_array[0] = testint;
+        test_array[1] = testchar;
+        test_array[2] = teststring;
+
+        return test_array;
+}
+
+
+void testQUEUE_LOAD_DATA(void)
 {
-        // test queue empty 
-        int array[] = {12,3,45,67,87,89,9};
-        Queue *queue = queue_init();
-        CU_ASSERT_EQUAL(0, queue->front);
-        CU_ASSERT_EQUAL(0, queue->back);
+        TestCase **tests = make_tests(NUM_TESTS);
 
-        // test initial data load
-        queue_add_data(queue, array, sizeof(array), sizeof(array[0]));
-        CU_ASSERT_TRUE(match(array, queue->array, sizeof(array), sizeof(array[0]), equal_int));
-        
-        // test queue_peek
-        int front_of_queue = 12;
-        CU_ASSERT_EQUAL(front_of_queue, to_int(queue_peek(queue)));
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                Queue *queue = queue_init();
+                queue_add_data(queue, tests[i]->test, tests[i]->data_size, tests[i]->elem_size);
+                CU_ASSERT_TRUE(match(tests[i]->test, queue->array, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
 
-        // test dequeue
-        dequeue(queue);
-        front_of_queue = 3;
-        CU_ASSERT_EQUAL(front_of_queue, to_int(queue_peek(queue)));
-        CU_ASSERT_EQUAL(1, queue->front);
+                queue_delete(queue);
+        }
 
-        queue_delete(queue);
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testQUEUE_PEEK(void)
+{
+        TestCase **tests = make_tests(NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                Queue *queue = queue_init();
+                queue_add_data(queue, tests[i]->test, tests[i]->data_size, tests[i]->elem_size);
+                CU_ASSERT_TRUE(match(tests[i]->test, queue->array, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+
+                void *peeked = queue_peek(queue);
+                void *should_be_front = tests[i]->test + tests[i]->elem_size * (LENGTH-LENGTH);
+                CU_ASSERT_TRUE(tests[i]->equal(peeked, should_be_front));
+
+                queue_delete(queue);
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testDEQUEUE(void)
+{
+
+        TestCase **tests = make_tests(NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+
+                Queue *queue = queue_init();
+                queue_add_data(queue, tests[i]->test, tests[i]->data_size, tests[i]->elem_size);
+                CU_ASSERT_TRUE(match(tests[i]->test, queue->array, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+
+                dequeue(queue);
+                void *peeked = queue_peek(queue);
+                void *should_be_front = tests[i]->test + tests[i]->elem_size * (LENGTH-(LENGTH-1));
+                CU_ASSERT_TRUE(tests[i]->equal(peeked, should_be_front));
+                CU_ASSERT_EQUAL(queue->front, LENGTH-(LENGTH-1));
+
+                queue_delete(queue);
+        }
+
+        clean_tests(tests, NUM_TESTS);
 }
 
 
@@ -59,7 +131,9 @@ int main(void)
         }
 
         // add tests
-        if (NULL == CU_add_test(suite, "Basic operations", testBASIC_QUEUE_OPERATIONS))
+        if (NULL == CU_add_test(suite, "Load data into queue", testQUEUE_LOAD_DATA) ||
+            NULL == CU_add_test(suite, "Peek returns front of queue", testQUEUE_PEEK) ||
+            NULL == CU_add_test(suite, "Pop removes element from top", testDEQUEUE))
         {
                 CU_cleanup_registry();
                 return CU_get_error();
