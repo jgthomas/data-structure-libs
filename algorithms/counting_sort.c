c810e94e7c5dc41cbaf05b08ae7e9691bc9df1cd
James Thomas <codeactor@protonmail.com>
2017-12-08 17:41:44 +0000

    general object counting sort added

--- algorithms/counting_sort.c
+++ algorithms/counting_sort.c
@@ -4,6 +4,8 @@
  *
  * */
 #include <stdio.h>
+#include "distribution_sort.h"
+#include "counting_sort.h"
 
 
 /**
@@ -40,3 +42,91 @@ void simple_counting_sort(int array[], int len, int max)
 }
 
 
+/**
+ * Stable version of algorithm that maintains the position
+ * of equal elements, although not strictly required for integers
+ *
+ * */
+void counting_sort(int array[], int len, int max)
+{
+        int map[max];
+        int sorted[len];
+
+        // initialise map
+        for (int i = 0; i < max; i++)
+        {
+                map[i] = 0;
+        }
+
+        // build map of input array
+        for (int i = 0; i < len; i++)
+        {
+                map[array[i]]++;
+        }
+
+        // adjust map to show output indicies
+        for (int i = 1; i < max; i++)
+        {
+                map[i] += map[i-1];
+        }
+
+        // walk back through original array, buliding sorted
+        // maintains stable ordering
+        for (int i = len-1; i >= 0; i--)
+        {
+                map[array[i]]--;
+                sorted[map[array[i]]] = array[i];
+        }
+
+        // copy sorted into original array
+        for (int i = 0; i < len; i++)
+        {
+                array[i] = sorted[i];
+        }
+}
+
+
+/**
+ * Provides a stable sort of an array of key-data associations.
+ *
+ * The array is comprised of Record structs with two fields
+ *     key  : the integer to sort
+ *     data : pointer to the associated data
+ *
+ * */
+void object_counting_sort(Record *record_array[], int len, int max)
+{
+        int map[max];
+        Record *sorted_records[len];
+
+        // initialise map
+        for (int i = 0; i < max; i++)
+        {
+                map[i] = 0;
+        }
+
+        // build map of input array
+        for (int i = 0; i < len; i++)
+        {
+                map[record_array[i]->key]++;
+        }
+
+        // adjust map to show output indicies
+        for (int i = 1; i < max; i++)
+        {
+                map[i] += map[i-1];
+        }
+
+        // walk back through original array, buliding sorted
+        for (int i = len-1; i >= 0; i--)
+        {
+                map[record_array[i]->key]--;
+                sorted_records[map[record_array[i]->key]] = record_array[i];
+        }
+
+        // copy sorted into original array
+        for (int i = 0; i < len; i++)
+        {
+                record_array[i] = sorted_records[i];
+        }
+}
--- algorithms/counting_sort.h
+++ algorithms/counting_sort.h
@@ -1,1 +1,10 @@
+#ifndef COUNTING_SORT_H
+#define COUNTING_SORT_H
+
+#include "distribution_sort.h"
+
 void simple_counting_sort(int array[], int len, int max);
+void counting_sort(int array[], int len, int max);
+void object_counting_sort(Record *record_array[], int len, int max);
+
+#endif
