f0aa6cca6d899889bda1707bebd79ac3f04786dc
James Thomas <codeactor@protonmail.com>
2017-12-08 17:42:17 +0000

    basic functions for generic distribution sorts

---
+++ algorithms/distribution_sort.c
@@ -0,0 +1,57 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include "distribution_sort.h"
+
+
+void build_records(Record *record_array[],
+                   int int_array[],
+                   void *data_array,
+                   size_t data_size,
+                   size_t elem_size)
+{
+        int len = data_size/elem_size;
+        
+        for (int i = 0; i < len; i++)
+        {
+                record_array[i] = malloc(sizeof(Record));
+                record_array[i]->key = int_array[i];
+                record_array[i]->data = data_array + elem_size * i;
+        }
+}
+
+
+void delete_records(Record *record_array[], int len)
+{
+        for (int i = 0; i < len; i++)
+        {
+                free(record_array[i]);
+        }
+}
+
+
+void print_records(Record *record_array[], int len, void (*print)(void *x))
+{
+        for (int i = 0; i < len; i++)
+        {
+                print(record_array[i]->data);
+                printf(" %d   ", record_array[i]->key);
+        }
+        printf("\n");
+}
+
+
+bool records_match_array(Record *record_array[],
+                         char *strings[],
+                         int len,
+                         bool (*equal)(void *x, void *y))
+{
+        for (int i = 0; i < len; i++)
+        {
+                if (!equal(strings[i], record_array[i]->data))
+                {
+                        return false;
+                }
+        }
+        return true;
+}
---
+++ algorithms/distribution_sort.h
@@ -0,0 +1,27 @@
+#ifndef DISTRIBUTION_SORT_H
+#define DISTRIBUTION_SORT_H
+
+#include <stdbool.h>
+
+typedef struct Record {
+        int key;
+        void *data;
+}
+Record;
+
+
+void build_records(Record *record_array[],
+                   int int_array[],
+                   void *data_array,
+                   size_t data_size,
+                   size_t elem_size);
+
+bool records_match_array(Record *record_array[],
+                         char *strings[],
+                         int len,
+                         bool (*equal)(void *x, void *y));
+
+void delete_records(Record *record_array[], int len);
+void print_records(Record *record_array[], int len, void (*print)(void *x));
+
+#endif
