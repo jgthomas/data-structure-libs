e6c23babadaaad0848a0d582bd17e2cb14f23445
James Thomas <codeactor@protonmail.com>
2017-12-15 18:06:07 +0000

    working treap implementation

--- data_structures/treap.c
+++ data_structures/treap.c
@@ -1,33 +1,124 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <stdbool.h>
+#include <string.h>
 #include <time.h>
 #include "treap.h"
 
 
-void treap_init(void)
+TreapNode *treap_init(void)
 {
         time_t t;
+        TreapNode *root = NULL;
+
+        // seed random number for priority
         srand((unsigned) time(&t));
+
+        return root;
 }
 
 
-TreapNode *treap_make_node(void *new_data)
+int priority(int max)
+{
+        return rand() % max;
+}
+
+
+TreapNode *treap_make_node(void *data, size_t data_size)
 {
         TreapNode *new_node = malloc(sizeof(*new_node));
 
         if (new_node == NULL)
         {
-                fprintf(stderr, "Failed to allocate memory\n");
+                fprintf(stderr, "Failed to allocate memory for node\n");
                 exit(EXIT_FAILURE);
         }
 
-        new_node->key = new_data;
+        new_node->key = malloc(data_size);
+
+        if (new_node->key == NULL)
+        {
+                fprintf(stderr, "Failed to allocate memory for data\n");
+                exit(EXIT_FAILURE);
+        }
+
+        memcpy(new_node->key, data, data_size);
+
         new_node->priority = priority(MAX);
+
         new_node->left = NULL;
         new_node->right = NULL;
 
         return new_node;
+}
+
+
+void treap_insert(TreapNode **root,
+                  void *new_data,
+                  size_t data_size,
+                  bool (*less_than)(void *x, void *y))
+{
+        if ((*root) == NULL)
+        {
+                (*root) = treap_make_node(new_data, data_size);
+        }
+        else if (less_than(new_data, (*root)->key))
+        {
+                treap_insert(&(*root)->left, new_data, data_size, less_than);
+
+                if ((*root)->left->priority > (*root)->priority)
+                {
+                        rotate_right(root);
+                }
+        }
+        else
+        {
+                treap_insert(&(*root)->right, new_data, data_size, less_than);
+
+                if ((*root)->right->priority > (*root)->priority)
+                {
+                        rotate_left(root);
+                }
+        }
+}
+
+/**
+ * Store the LEFT child as the new_parent
+ * Store the new parent's original RIGHT node
+ *
+ * Set the old parent as the new parent's RIGHT node
+ * Set the stored RIGHT node as old parent's LEFT node
+ * Set the new parent as the parent node
+ *
+ * */
+void rotate_right(TreapNode **node)
+{
+        TreapNode *new_parent = (*node)->left;
+        TreapNode *old_child_right = new_parent->right;
+
+        new_parent->right = (*node);
+        (*node)->left = old_child_right;
+        (*node) = new_parent;
+}
+
+
+/**
+ * Store the RIGHT child as the new_parent
+ * Store the new parent's original LEFT node
+ *
+ * Set the old parent as the new parent's LEFT node
+ * Set the stored LEFT node as old parent's RIGHT node
+ * Set the new parent as the parent node
+ *
+ * */
+void rotate_left(TreapNode **node)
+{
+        TreapNode *new_parent = (*node)->right;
+        TreapNode *old_child_left = new_parent->left;
+
+        new_parent->left = (*node);
+        (*node)->right = old_child_left;
+        (*node) = new_parent;
 }
 
 
@@ -42,84 +133,15 @@ void treap_load_data(TreapNode **head,
         for (int i = 0; i < len; i++)
         {
                 void *next = new_data + elem_size * i;
-                treap_insert(head, next, less_than);
+                treap_insert(head, next, data_size, less_than);
         }
 }
 
 
-void treap_insert(TreapNode **node,
-                  void *new_data,
-                  bool (*less_than)(void *first, void *second))
+void treap_delete_node(TreapNode *node)
 {
-        if ((*node) == NULL)
-        {
-                (*node) = treap_make_node(new_data);
-        }
-        else if (less_than(new_data, (*node)->key))
-        {
-                treap_insert(&(*node)->left, new_data, less_than);
-
-                if ((*node)->left->priority > (*node)->priority)
-                {
-                        rotate_right(node);
-                }
-        }
-        else
-        {
-                treap_insert(&(*node)->right, new_data, less_than);
-
-                if ((*node)->right->priority > (*node)->priority)
-                {
-                        rotate_left(node);
-                }
-        }
-}
-
-
-void rotate_right(TreapNode **node)
-{
-        TreapNode *new_parent = (*node)->left;
-        TreapNode *tmp = new_parent->right;
-
-        new_parent->right = (*node);
-        (*node)->left = tmp;
-        (*node) = new_parent;
-}
-
-
-void rotate_left(TreapNode **node)
-{
-        TreapNode *new_parent = (*node)->right;
-        TreapNode *tmp = new_parent->left;
-
-        new_parent->left = (*node);
-        (*node)->right = tmp;
-        (*node) = new_parent;
-}
-
-
-bool treap_search(TreapNode **node,
-                  void *data,
-                  bool (*equals)(void *first, void *second),
-                  bool (*less_than)(void *first, void *second))
-{
-        if ((*node) != NULL)
-        {
-                if (equals((*node)->key, data))
-                {
-                        return true;
-                }
-                else if (less_than(data, (*node)->key))
-                {
-                        return treap_search(&(*node)->left, data, equals, less_than);
-                }
-                else
-                {
-                        return treap_search(&(*node)->right, data, equals, less_than);
-                }
-        }
-
-        return false;
+        free(node->key);
+        free(node);
 }
 
 
@@ -129,28 +151,8 @@ void treap_delete(TreapNode *node)
         {
                 treap_delete(node->left);
                 treap_delete(node->right);
-                free(node);
+                treap_delete_node(node);
         }
-}
-
-
-int priority(int max)
-{
-        return rand() % max;
-}
-
-
-void treap_in_order_print(TreapNode *node, void (*print)(void *x))
-{
-        if (node == NULL)
-        {
-                return;
-        }
-
-        treap_in_order_print(node->left, print);
-        print(node->key);
-        printf(" ");
-        treap_in_order_print(node->right, print);
 }
 
 
@@ -167,11 +169,79 @@ void treap_visualise(TreapNode *node,
 
 	    for (int i = 0; i < level; i++)
 	    {
-	            printf("     ");
+	            printf("       ");
 	    }
 	    print(node->key);
-        printf("(%d)", node->priority);
+            printf("(%d)", node->priority);
 	    printf("\n\n");
 
 	    treap_visualise(node->left, level+1, print);
+}
+
+
+/**
+ * InOrder traversal
+ *
+ * Traverse the tree from 'left to right' with root in the middle
+ *
+ * left subtree -> root -> right subtree
+ *
+ * Prints all values in tree in ascending order
+ *
+ * */
+void treap_in_order_print(TreapNode *node, void (*print)(void *x))
+{
+        if (node == NULL)
+        {
+                return;
+        }
+
+        treap_in_order_print(node->left, print);
+        print(node->key);
+        printf(" ");
+        treap_in_order_print(node->right, print);
+}
+
+
+/**
+ * PreOrder traversal
+ *
+ * Traverse the tree from the 'top down'
+ *
+ * root -> left subtree -> right subtree
+ *
+ * */
+void treap_pre_order_print(TreapNode *node, void (*print)(void *x))
+{
+        if (node == NULL)
+	{
+	        return;
+	}
+
+        print(node->key);
+        printf(" ");
+        treap_pre_order_print(node->left, print);
+        treap_pre_order_print(node->right, print);
+}
+
+
+/**
+ * PostOrder traversal
+ *
+ * Traverse the tree from the 'bottom up'
+ *
+ * left subtree -> right subtree -> root
+ *
+ * */
+void treap_post_order_print(TreapNode *node, void (*print)(void *x))
+{
+        if (node == NULL)
+        {
+                return;
+        }
+
+        treap_post_order_print(node->left, print);
+        treap_post_order_print(node->right, print);
+        print(node->key);
+        printf(" ");
 }
--- data_structures/treap.h
+++ data_structures/treap.h
@@ -13,23 +13,23 @@ typedef struct treap_node {
 }
 TreapNode;
 
+
+TreapNode *treap_init(void);
 int priority(int max);
-void treap_init(void);
-TreapNode *treap_make_node(void *new_data);
-void treap_delete(TreapNode *node);
+TreapNode *BST_make_node(void *data, size_t data_size);
 void rotate_right(TreapNode **node);
 void rotate_left(TreapNode **node);
+void treap_delete_node(TreapNode *node);
+void treap_delete(TreapNode *node);
 
 void treap_in_order_print(TreapNode *node, void (*print)(void *x));
+void treap_pre_order_print(TreapNode *node, void (*print)(void *x));
+void treap_post_order_print(TreapNode *node, void (*print)(void *x));
 
-bool treap_search(TreapNode **root,
-            void *data,
-            bool (*equals)(void *first, void *second),
-            bool (*less_than)(void *first, void *second));
-
-void treap_insert(TreapNode **node,
+void treap_insert(TreapNode **root,
                   void *new_data,
-                  bool (*less_than)(void *first, void *second));
+                  size_t data_size,
+                  bool (*less_than)(void *x, void *y));
 
 void treap_load_data(TreapNode **head,
                      void *new_data,
