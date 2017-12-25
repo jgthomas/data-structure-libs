e82a8684f504e108e656c8efb388d37418fe44a1
James Thomas <codeactor@protonmail.com>
2017-12-25 15:18:29 +0000

    test for repeated elements

--- tests/test_sort_defs.c
+++ tests/test_sort_defs.c
@@ -5,6 +5,10 @@
 #include "tests/test_sort_defs.h"
 
 
+/**
+ * Basic sorting
+ *
+ * */
 int test_int[] = {1,7,6,5,22,8,1,15,74};
 char test_char[] = {'c','d','b','f','g','a'};
 char *test_string[] = {"abacus",
@@ -38,6 +42,10 @@ TestCase **basic_tests(int num_tests)
 }
 
 
+/**
+ * Empty array
+ *
+ * */
 int empty_int[] = {};
 char empty_char[] = {};
 char *empty_string[] = {};
@@ -58,6 +66,10 @@ TestCase **empty_test(int num_tests)
 }
 
 
+/**
+ * Single element
+ *
+ * */
 int single_int[] = {8};
 char single_char[] = {'c'};
 char *single_string[] = {"doggy"};
@@ -69,6 +81,42 @@ TestCase **single_element_test(int num_tests)
         TestCase *testint = new_test(single_int, single_int, sizeof(single_int), sizeof(int), equal_int, less_than_int, print_int);
         TestCase *testchar = new_test(single_char, single_char, sizeof(single_char), sizeof(char), equal_char, less_than_char, print_char);
         TestCase *teststring = new_test(single_string, single_string, sizeof(single_string), sizeof(char *), equal_string, less_than_string, print_string);
+
+        tests[0] = testint;
+        tests[1] = testchar;
+        tests[2] = teststring;
+
+        return tests;
+}
+
+
+/**
+ * Repeat elements
+ *
+ * */
+int repeat_int[] = {2,1,1,2,0};
+char repeat_char[] = {'c','c','w','w','p'};
+char *repeat_string[] = {"donkey",
+                         "donkey",
+                         "giraffe",
+                         "giraffe",
+                         "ape"};
+
+int repeat_int_answer[] = {0,1,1,2,2};
+char repeat_char_answer[] = {'c','c','p','w','w'};
+char *repeat_string_answer[] = {"ape",
+                                "donkey",
+                                "donkey",
+                                "giraffe",
+                                "giraffe"};
+
+TestCase **repeat_element_test(int num_tests)
+{
+        TestCase **tests = init_tests(num_tests);
+
+        TestCase *testint = new_test(repeat_int, repeat_int_answer, sizeof(repeat_int), sizeof(int), equal_int, less_than_int, print_int);
+        TestCase *testchar = new_test(repeat_char, repeat_char_answer, sizeof(repeat_char), sizeof(char), equal_char, less_than_char, print_char);
+        TestCase *teststring = new_test(repeat_string, repeat_string_answer, sizeof(repeat_string), sizeof(char *), equal_string, less_than_string, print_string);
 
         tests[0] = testint;
         tests[1] = testchar;
--- tests/test_sort_defs.h
+++ tests/test_sort_defs.h
@@ -6,5 +6,6 @@ enum {NUM_TESTS = 3};
 TestCase **basic_tests(int num_tests);
 TestCase **empty_test(int num_tests);
 TestCase **single_element_test(int num_tests);
+TestCase **repeat_element_test(int num_tests);
 
 #endif
