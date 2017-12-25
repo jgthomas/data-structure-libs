36240a26aec1be3271de7156f2b33b589d4554e1
James Thomas <codeactor@protonmail.com>
2017-12-25 13:49:46 +0000

    using centralised tests

--- tests/test_bubble_sort/test_bubble_sort.c
+++ tests/test_bubble_sort/test_bubble_sort.c
@@ -9,42 +9,9 @@
 #include "algorithms/bubble_sort.h"
 
 
-int test_int[] = {1,7,6,5,22,8,1,15,74};
-char test_char[] = {'c','d','b','f','g','a'};
-char *test_string[] = {"abacus",
-                       "zebra",
-                       "doggy",
-                       "thinning",
-                       "soldier"};
-
-int answer_int[] = {1,1,5,6,7,8,15,22,74};
-char answer_char[] = {'a','b','c','d','f','g'};
-char *answer_string[] = {"abacus",
-                         "doggy",
-                         "soldier",
-                         "thinning",
-                         "zebra"};
-
-
-TestCase **make_tests(int num_tests)
-{        
-        TestCase **test_array = init_tests(num_tests);
-
-        TestCase *testint = new_test(test_int, answer_int, sizeof(test_int), sizeof(int), equal_int, less_than_int, print_int);
-        TestCase *testchar = new_test(test_char, answer_char, sizeof(test_char), sizeof(char), equal_char, less_than_char, print_char);
-        TestCase *teststring = new_test(test_string, answer_string, sizeof(test_string), sizeof(char *), equal_string, less_than_string, print_string);
-
-        test_array[0] = testint;
-        test_array[1] = testchar;
-        test_array[2] = teststring;
-
-        return test_array;
-}
-
-
 void testBASIC_SORTING(void)
 {
-        TestCase **tests = make_tests(NUM_TESTS);
+        TestCase **tests = basic_tests(NUM_TESTS);
 
         for (int i = 0; i < NUM_TESTS; i++)
         {
