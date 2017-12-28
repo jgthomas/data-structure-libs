bcbd2ad6b52cc12239389efa95b5828c5326496d
James Thomas <codeactor@protonmail.com>
2017-12-28 13:53:25 +0000

    heapsort using central test defs

--- tests/test_heapsort/test_heapsort.c
+++ tests/test_heapsort/test_heapsort.c
@@ -5,56 +5,147 @@
 #include "tools/printing.h"
 #include "tools/comparison.h"
 #include "tests/shared_test.h"
+#include "tests/comparison_sort_tests.h"
 #include "algorithms/heapsort.h"
 
 
-enum {NUM_TESTS = 3};
-
-
-int test_int[] = {1,7,6,5,22,8,1,15,74};
-char test_char[] = {'c','d','b','f','g','a'};
-char *test_string[] = {"abacus",
-                       "zebra",
-                       "doggy",
-                       "thinning",
-                       "soldier"};
-
-int answer_int[] = {1,1,5,6,7,8,15,22,74};
-char answer_char[] = {'a','b','c','d','f','g'};
-char *answer_string[] = {"abacus",
-                         "doggy",
-                         "soldier",
-                         "thinning",
-                         "zebra"};
-
-
-TestCase **make_tests(int num_tests)
-{        
-        TestCase **test_array = init_tests(num_tests);
-
-        TestCase *testint = new_test(test_int, answer_int, sizeof(test_int), sizeof(int), equal_int, more_than_int, print_int);
-        TestCase *testchar = new_test(test_char, answer_char, sizeof(test_char), sizeof(char), equal_char, more_than_char, print_char);
-        TestCase *teststring = new_test(test_string, answer_string, sizeof(test_string), sizeof(char *), equal_string, more_than_string, print_string);
-
-        test_array[0] = testint;
-        test_array[1] = testchar;
-        test_array[2] = teststring;
-
-        return test_array;
+/**
+ * Override comparison function as heapsort uses a max-heap
+ *
+ * */
+void change_comparison(TestCase **tests, int num_tests)
+{
+        for (int i = 0; i < num_tests; i++)
+        {
+                if (i == 0)
+                {
+                        tests[i]->compare = more_than_int;
+                }
+                else if (i == 1)
+                {
+                        tests[i]->compare = more_than_char;
+                }
+                else if (i == 2)
+                {
+                        tests[i]->compare = more_than_string;
+                }
+        }
 }
 
 
-void testHEAPSORT(void)
+void testODD_NO_ELEMENTS(void)
 {
-        TestCase **tests = make_tests(NUM_TESTS);
+        TestCase **tests = odd_test(NUM_TESTS);
+
+        change_comparison(tests, NUM_TESTS);
 
         for (int i = 0; i < NUM_TESTS; i++)
         {
                 CU_ASSERT_FALSE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
-                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
                 heapsort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
                 CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
-                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testEVEN_NO_ELEMENTS(void)
+{
+        TestCase **tests = even_test(NUM_TESTS);
+
+        change_comparison(tests, NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_FALSE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                heapsort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testEMPTY_LIST(void)
+{
+        TestCase **tests = empty_test(NUM_TESTS);
+
+        change_comparison(tests, NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                heapsort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testSINGLE_ELEMENT(void)
+{
+        TestCase **tests = single_element_test(NUM_TESTS);
+
+        change_comparison(tests, NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                heapsort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testREPEAT_ELEMENT(void)
+{
+        TestCase **tests = repeat_element_test(NUM_TESTS);
+
+        change_comparison(tests, NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_FALSE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                heapsort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testREPEAT_SINGLE_ELEMENT(void)
+{
+        TestCase **tests = single_rep_test(NUM_TESTS);
+
+        change_comparison(tests, NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                heapsort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testSORTED(void)
+{
+        TestCase **tests = sorted_test(NUM_TESTS);
+
+        change_comparison(tests, NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                heapsort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
         }
 
         clean_tests(tests, NUM_TESTS);
@@ -73,7 +164,7 @@ int main(void)
         }
 
         // add suite
-        suite = CU_add_suite("Sorting Algorithms", 0, 0);
+        suite = CU_add_suite("Heapsort", 0, 0);
         if (NULL == suite)
         {
                 CU_cleanup_registry();
@@ -81,7 +172,13 @@ int main(void)
         }
 
         // add tests
-        if (NULL == CU_add_test(suite, "Heapsort", testHEAPSORT))
+        if (NULL == CU_add_test(suite, "Odd number of elements", testODD_NO_ELEMENTS) ||
+            NULL == CU_add_test(suite, "Even number of elements", testEVEN_NO_ELEMENTS) ||
+            NULL == CU_add_test(suite, "Zero elements", testEMPTY_LIST) ||
+            NULL == CU_add_test(suite, "Single element", testSINGLE_ELEMENT) ||
+            NULL == CU_add_test(suite, "Repeat elements", testREPEAT_ELEMENT) ||
+            NULL == CU_add_test(suite, "Len 2, all the same", testREPEAT_SINGLE_ELEMENT) ||
+            NULL == CU_add_test(suite, "Already sorted", testSORTED))
         {
                 CU_cleanup_registry();
                 return CU_get_error();
