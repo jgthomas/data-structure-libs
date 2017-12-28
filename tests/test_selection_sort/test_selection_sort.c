cf777214943a712bbeb1de0f12f55b153202e58c
James Thomas <codeactor@protonmail.com>
2017-12-28 12:31:27 +0000

    selection sort using new tests

--- tests/test_selection_sort/test_selection_sort.c
+++ tests/test_selection_sort/test_selection_sort.c
@@ -5,48 +5,13 @@
 #include "tools/printing.h"
 #include "tools/comparison.h"
 #include "tests/shared_test.h"
+#include "tests/comparison_sort_tests.h"
 #include "algorithms/selection_sort.h"
 
 
-enum {NUM_TESTS = 3};
-
-
-int test_int[] = {1,7,6,5,22,8,1,15,74};
-char test_char[] = {'c','d','b','f','g','a'};
-char *test_string[] = {"abacus",
-                       "zebra",
-                       "doggy",
-                       "thinning",
-                       "soldier"};
-
-int answer_int[] = {1,1,5,6,7,8,15,22,74};
-char answer_char[] = {'a','b','c','d','f','g'};
-char *answer_string[] = {"abacus",
-                         "doggy",
-                         "soldier",
-                         "thinning",
-                         "zebra"};
-
-
-TestCase **make_tests(int num_tests)
-{        
-        TestCase **test_array = init_tests(num_tests);
-
-        TestCase *testint = new_test(test_int, answer_int, sizeof(test_int), sizeof(int), equal_int, less_than_int, print_int);
-        TestCase *testchar = new_test(test_char, answer_char, sizeof(test_char), sizeof(char), equal_char, less_than_char, print_char);
-        TestCase *teststring = new_test(test_string, answer_string, sizeof(test_string), sizeof(char *), equal_string, less_than_string, print_string);
-
-        test_array[0] = testint;
-        test_array[1] = testchar;
-        test_array[2] = teststring;
-
-        return test_array;
-}
-
-
-void testBASIC_SORTING(void)
+void testODD_NO_ELEMENTS(void)
 {
-        TestCase **tests = make_tests(NUM_TESTS);
+        TestCase **tests = odd_test(NUM_TESTS);
 
         for (int i = 0; i < NUM_TESTS; i++)
         {
@@ -60,6 +25,107 @@ void testBASIC_SORTING(void)
         clean_tests(tests, NUM_TESTS);
 }
 
+
+void testEVEN_NO_ELEMENTS(void)
+{
+        TestCase **tests = even_test(NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_FALSE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+                selection_sort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testEMPTY_LIST(void)
+{
+        TestCase **tests = empty_test(NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+                selection_sort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testSINGLE_ELEMENT(void)
+{
+        TestCase **tests = single_element_test(NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+                selection_sort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testREPEAT_ELEMENT(void)
+{
+        TestCase **tests = repeat_element_test(NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_FALSE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+                selection_sort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testREPEAT_SINGLE_ELEMENT(void)
+{
+        TestCase **tests = single_rep_test(NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+                selection_sort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
+
+
+void testSORTED(void)
+{
+        TestCase **tests = sorted_test(NUM_TESTS);
+
+        for (int i = 0; i < NUM_TESTS; i++)
+        {
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+                selection_sort(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->compare);
+                CU_ASSERT_TRUE(arrays_match(tests[i]->test, tests[i]->answer, tests[i]->data_size, tests[i]->elem_size, tests[i]->equal));
+                //print_sequence(tests[i]->test, tests[i]->data_size, tests[i]->elem_size, tests[i]->print);
+        }
+
+        clean_tests(tests, NUM_TESTS);
+}
 
 
 int main(void)
@@ -82,7 +148,13 @@ int main(void)
         }
 
         // add tests
-        if (NULL == CU_add_test(suite, "Basic sorting", testBASIC_SORTING))
+        if (NULL == CU_add_test(suite, "Odd number of elements", testODD_NO_ELEMENTS) ||
+            NULL == CU_add_test(suite, "Even number of elements", testEVEN_NO_ELEMENTS) ||
+            NULL == CU_add_test(suite, "Zero elements", testEMPTY_LIST) ||
+            NULL == CU_add_test(suite, "Single element", testSINGLE_ELEMENT) ||
+            NULL == CU_add_test(suite, "Repeat elements", testREPEAT_ELEMENT) ||
+            NULL == CU_add_test(suite, "Len 2, all the same", testREPEAT_SINGLE_ELEMENT) ||
+            NULL == CU_add_test(suite, "Already sorted", testSORTED))
         {
                 CU_cleanup_registry();
                 return CU_get_error();
