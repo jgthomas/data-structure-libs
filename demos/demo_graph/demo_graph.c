fe0c64a1fe7b72d124a86227ddb06b49714fbc62
James Thomas <codeactor@protonmail.com>
2018-01-10 22:34:48 +0000

    basic user options added

--- demos/demo_graph/demo_graph.c
+++ demos/demo_graph/demo_graph.c
@@ -1,14 +1,44 @@
 #include <stdio.h>
+#include <string.h>
+#include "tools/get_input.h"
 #include "graph.h"
 #include "adjacency_matrix.h"
 
 
 int main(void)
 {
-        printf("adjacency matrix\n");
-        vertex_report(2);
-        printf("\n");
-        matrix_report(0, 4);
-        list_vertices();
-        list_edges();
+        int choice = 0;
+
+        printf("\nPick an Implementation\n\n");
+
+        while (choice < 1 || choice > 2)
+        {
+                choice = get_int("(1) Adjacency Matrix or (2) Edge list");
+        }
+
+        if (choice == 1)
+        {
+                printf("\nAdjacency Matrix\n\n");
+
+                char command[20];
+
+                while (1)
+                {
+                        printf("Enter a command: ");
+                        fgets(command, sizeof(command), stdin);
+
+                        if (strncmp(command, "vertex", 6) == 0)
+                        {
+                                list_vertices();
+                        }
+                        else if (strncmp(command, "edges", 5) == 0)
+                        {
+                                list_edges();
+                        }
+                        else if (strncmp(command, "quit", 4) == 0)
+                        {
+                                break;
+                        }
+                }
+        }
 }
